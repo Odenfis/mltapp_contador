fd4918431cfb699b1460da82d5cf68d5a021be18
walter <wmoncada@tecsup.edu.pe>
2023-10-03 16:44:43 -0500

    03102023_1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,26 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const HomeScreen());
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  //en una statefulwidget se declara variables antes de su build
+  int contador = 20;
+
+  @override
   Widget build(BuildContext context) {
+    //Declaracion de variables
+    const fontSizeVar22 = TextStyle(fontSize: 22);
+    
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Titulo de la App'),
         elevation: 10,
       ),
       backgroundColor:Colors.cyanAccent,      
-      body: const Center(
+      body:  Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text('Clic para cambiar los datos del contador',style: TextStyle(fontSize: 22)),
-              Text('20',style: TextStyle(fontSize: 40),)
+              const Text('Clic para cambiar los datos del contador',style: fontSizeVar22,textAlign: TextAlign.center,),
+              Text('$contador',style: fontSizeVar22,)
             ],
           ),
-      )      
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:(){
+        
+          setState(() {contador++;});
+        },
+        child: const Icon(Icons.plus_one),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          
      
     );
   }
