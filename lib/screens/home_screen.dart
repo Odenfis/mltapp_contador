ee8cbb03efafefd8d968cdb7171e8636f4f4d746
walter <wmoncada@tecsup.edu.pe>
2023-10-03 15:36:12 -0500

    03102023

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,11 +7,22 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor:Colors.cyanAccent,      
-      body: Center(
-        child: Text('Hola a todos mis amiguitos'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Titulo de la App'),
+        elevation: 10,
       ),
+      backgroundColor:Colors.cyanAccent,      
+      body: const Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Clic para cambiar los datos del contador',style: TextStyle(fontSize: 22)),
+              Text('20',style: TextStyle(fontSize: 40),)
+            ],
+          ),
+      )      
+     
     );
   }
 }
